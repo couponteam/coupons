24210de16b6a7b8e0ef0aa47ecee876bbfaceb64
huangpu <hpdroid@yahoo.com>
2017-05-17 00:13:04 +0800

    添加七牛上传图片功能

---
+++ src/main/java/com/masou/coupon/action/erpapi/ErpResourceController.java
@@ -0,0 +1,65 @@
+package com.masou.coupon.action.erpapi;
+
+import com.masou.coupon.common.struct.Result;
+import com.masou.coupon.common.utils.ResultHelper;
+import com.masou.coupon.data.models.Resource;
+import com.masou.coupon.exception.UserException;
+import com.masou.coupon.service.QiniuService;
+import com.masou.coupon.utils.MD5Util;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * Created by Paul on 2017/5/9.
+ */
+@RestController
+@RequestMapping("/management/api/resource")
+public class ErpResourceController {
+    @Autowired
+    private MD5Util md5Util;
+
+    @Autowired
+    private QiniuService qiniuService;
+
+    @ApiOperation("插入图片")
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public Result insert(@RequestParam(value = "name") String name,
+                         @RequestParam(value = "file") MultipartFile file) {
+
+        String md5 = null;
+        try {
+            md5 = md5Util.MD5File(file.getBytes());
+            String fileName = file.getOriginalFilename();
+            String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+            if (extension.equals("")) {
+                throw new UserException("请先选择文件");
+            } else {
+                switch (extension.toLowerCase()) {
+                    case "jpg":
+                    case "jpeg":
+                    case "png":
+                        break;
+                    default:
+                        throw new UserException("不支持的文件格式");
+
+                }
+            }
+            String fileKey = String.format("%s.%s", md5, extension);
+            boolean uploadResult = qiniuService.upload(file.getBytes(), fileKey);
+            //保存
+
+            return ResultHelper.genResultWithSuccess(fileKey);
+        } catch (IOException e) {
+            throw new UserException("异常错误");
+        }
+    }
+
+
+}
---
+++ src/main/java/com/masou/coupon/data/models/Resource.java
@@ -0,0 +1,86 @@
+package com.masou.coupon.data.models;
+
+public class Resource {
+    private Integer id;
+
+    private String name;
+
+    private String path;
+
+    private String md5;
+
+    private String extend;
+
+    private Long size;
+
+    private String group;
+
+    private String createAt;
+
+    public Resource() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path == null ? null : path.trim();
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5 == null ? null : md5.trim();
+    }
+
+    public String getExtend() {
+        return extend;
+    }
+
+    public void setExtend(String extend) {
+        this.extend = extend == null ? null : extend.trim();
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group == null ? null : group.trim();
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt == null ? null : createAt.trim();
+    }
+}
